6a2d2005af228e8fca7d5a822a054c5594166288
KhaerulArizki <arizkikhaerul@gmail.com>
2025-12-02 01:28:30 +0700

    Create koneksi.php

---
+++ PRAKTIKUM_7/kodingan/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "foto";
+
+$koneksi = new mysqli($host, $user, $pass, $db);
+
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
+}
+
+
+$koneksi->set_charset("utf8mb4");
